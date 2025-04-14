e656842bee4bf3cc63a1be005c720cf4bd4b3c69
Sebastian Corcino <corcinosebastian@outlook.com>
2025-04-13 20:14:17 -0800

    additional output examples

--- services/demo.py
+++ services/demo.py
@@ -7,21 +7,27 @@ from injestors.grpc_web_client import GrpcWebClient
 
 if __name__ == "__main__":
     
-    # # Update to your router id
+    # Update to your router id
     router_id = "Router-010000000000000000000000"
     client = GrpcWebClient()
     response = client.get_wifi_status(router_id)
     print(response)
+    with open("get_wifi_status.txt", "w") as f:
+        f.write(str(response))
    
-    # # Update to your terminal id
+    # Update to your terminal id
     terminal_id = "ut01000000-00000000-00000000"
     client = GrpcWebClient()
     response = client.get_dish_status(terminal_id)
     print(response)
+    with open("get_dish_status.txt", "w") as f:
+        f.write(str(response))
 
     # Update to your terminal id
     terminal_id = "ut01000000-00000000-00000000"
     client = GrpcWebClient()
     response = client.get_dish_telemetry(terminal_id)
     print(response)
+    with open("get_dish_telemetry.txt", "w") as f:
+        f.write(str(response))
    
